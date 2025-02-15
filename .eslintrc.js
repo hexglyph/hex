3d3f44152d1ba2878857355bf6814f324fcc983d
Daniel Niemietz Braz <dnbraz@prodam.sp.gov.br>
2025-02-15 03:33:29 -0300

    configura ESLint para TypeScript e React, adiciona regras personalizadas

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
-  extends: ["next/core-web-vitals"],
-}
-
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended'
+  ],
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/no-unescaped-entities': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
