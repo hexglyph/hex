10c35f7d5408b3b72a38fe3c3534b45653dfeb80
Daniel Niemietz Braz <dnbraz@prodam.sp.gov.br>
2025-02-16 06:12:23 -0300

    remove componente Testimonials e desativa regra ESLint para permitir 'any' em TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   }
 };
